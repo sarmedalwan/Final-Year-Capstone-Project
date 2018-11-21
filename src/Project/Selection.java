b7a7da1c526d203e7ceb19275a09c030ced533a9
Alwan, Sarmed A <sa16566@essex.ac.uk>
2018-11-21 20:09:27 +0000

    Handles the selection of units on the board

---
+++ src/Project/Selection.java
@@ -0,0 +1,20 @@
+package Project;
+
+import static Project.GameState.columns;
+import static Project.GameState.rows;
+
+public class Selection {
+    public static Unit selected(Unit[][] gameGrid){
+        for (int i = 0; i < columns; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                if (gameGrid[i][j].selected = true){
+                    gameGrid[i][j].setSelection(false);
+                    return gameGrid[i][j];
+                }
+            }
+        }
+        return null;
+    }
+}
